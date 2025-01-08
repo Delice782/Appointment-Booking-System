3ffbe23534b36bb2e194ae8fec63e099d62cd753
Delice Ishimwe <154625118+Delice782@users.noreply.github.com>
2025-01-08 00:38:33 +0000

    Update Appointment.java

--- Appointment.java
+++ Appointment.java
@@ -1,1 +1,25 @@
+import java.time.LocalDateTime;
+
+public class Appointment {
+    private String description;
+    private LocalDateTime dateTime;
+
+    public Appointment(String description, LocalDateTime dateTime) {
+        this.description = description;
+        this.dateTime = dateTime;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    @Override
+    public String toString() {
+        return "Appointment [description=" + description + ", dateTime=" + dateTime + "]";
+    }
+}
 
