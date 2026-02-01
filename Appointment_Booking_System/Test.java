b42317ce147b3deab7d789bff6d61d16ce7698ae
Delice Ishimwe <154625118+Delice782@users.noreply.github.com>
2026-02-01 23:58:20 +0000

    Update Test.java

--- Appointment_Booking_System/Test.java
+++ Appointment_Booking_System/Test.java
@@ -1,4 +1,4 @@
-  
+   
 /**
  * A class that interacts with the user, allowing them to book an appointment for a particular day/time,
  * cancel an appointment, reschedule an appointment, or display the list of appointments.
@@ -180,6 +180,7 @@ public class Test {
         while (running);
     }
 }
+
 
 
 
